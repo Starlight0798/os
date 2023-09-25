5dbefd37da701aa9e12d09381259a831ae07a1f2
wzyAtwf <1503681214@qq.com>
2023-09-25 12:39:32 +0800

    Update trap.c

--- riscv64-ucore-labcodes/lab1/kern/trap/trap.c
+++ riscv64-ucore-labcodes/lab1/kern/trap/trap.c
@@ -106,7 +106,7 @@ void interrupt_handler(struct trapframe *tf) {
             // In fact, Call sbi_set_timer will clear STIP, or you can clear it
             // directly.
             // cprintf("Supervisor timer interrupt\n");
-             /* LAB1 EXERCISE2   2113997 :  */
+             /* LAB1 EXERCISE2   2112474 :  */
             clock_set_next_event();
             ticks++;
             if(ticks%TICK_NUM == 0){
@@ -155,7 +155,7 @@ void exception_handler(struct trapframe *tf) {
             break;
 	case CAUSE_ILLEGAL_INSTRUCTION:
              // 非法指令异常处理
-             /* LAB1 CHALLENGE3   2113997 : */
+             /* LAB1 CHALLENGE3   2112474 : */
             /*(1)输出指令异常类型（ Illegal instruction）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
@@ -167,7 +167,7 @@ void exception_handler(struct trapframe *tf) {
             break;
         case CAUSE_BREAKPOINT:
             //断点异常处理
-            /* LAB1 CHALLLENGE3   2113997 : */
+            /* LAB1 CHALLLENGE3   2112474 : */
             /*(1)输出指令异常类型（ breakpoint）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
